83b05635f512f82a8359686cef73cc7b948efd98
Claudiano <55771749+ClaudianoLeonardo@users.noreply.github.com>
2021-08-15 22:33:13 -0300

    atualizado

--- interpretador.cpp
+++ interpretador.cpp
@@ -8,6 +8,11 @@
 #include<putvoxel.h>
 #include<cutvoxel.h>
 #include<putbox.h>
+#include<cutbox.h>
+#include<putsphere.h>
+#include<cutsphere.h>
+#include<putellipsoid.h>
+#include<cutellipsoid.h>
 Interpretador::Interpretador(){
 
 
@@ -27,7 +32,7 @@ std::vector<FiguraGeometrica*> Interpretador::Parse(std::string arq)
     fin.open(arq);
 
     if(!fin.is_open()){
-        std :: cout<< "Arquivo não abriu " << std::endl;
+        std :: cout<< "Arquivo nao abriu " << std::endl;
         exit(0);
     }
 
@@ -55,14 +60,54 @@ std::vector<FiguraGeometrica*> Interpretador::Parse(std::string arq)
                         ss >> x0 >> y0 >> z0;
                         Figuras.push_back(new CutVoxel(x0,y0,z0));
                         std::cout<<"voxel cortado"<< std::endl;
-                      }
-                    else if(token.compare("putbox")==0){
-                        int x0,y0,z0,x1,y1,z1;
-                        float r, g, b, a;
-                        ss >> x0 >> x1 >> y0 >> y1 >> z0 >> z1 >> r >> g >> b >> a;
-                        Figuras.push_back(new PutBox(x0,x1,y0,y1,z0,z1,r, g, b, a));
+
 
                 }
+
+
+                     else if(token.compare("putbox")==0){
+                                       int x0,y0,z0,x1,y1,z1;
+                                       float r, g, b, a;
+                                       ss >> x0 >> x1 >> y0 >> y1 >> z0 >> z1 >> r >> g >> b >> a;
+                                       Figuras.push_back(new PutBox(x0,x1,y0,y1,z0,z1,r, g, b, a));
+
+                               }
+                    else if(token.compare("cutbox")==0){
+                        int x0,y0,z0,x1,y1,z1;
+                        ss >> x0 >> x1 >> y0 >> y1 >> z0 >> z1;
+                        Figuras.push_back(new CutBox(x0,x1,y0,y1,z0,z1));
+                  }
+
+                    else if(token.compare("putsphere")==0){
+                        int xcenter,ycenter,zcenter,radius;
+                        float r, g, b, a;
+                        ss >> xcenter >> ycenter >> zcenter >> radius >> r >> g >> b >> a;
+                        Figuras.push_back(new PutSphere(xcenter, ycenter, zcenter, radius, r, g, b, a ));
+
+                }
+
+                    else if(token.compare("cutsphere")==0){
+                        int xcenter,ycenter,zcenter,radius;
+                        ss >> xcenter >> ycenter >> zcenter >> radius;
+                        Figuras.push_back(new CutSphere(xcenter, ycenter, zcenter, radius));
+
+                }
+
+                    else if(token.compare("putellipsoid")==0){
+                        int xcenter, ycenter, zcenter, rx, ry, rz;
+                        float r, g, b, a;
+                        ss >> xcenter >> ycenter >> zcenter >> rx >> ry >> rz >> r >> g >> b >> a;
+                        Figuras.push_back(new PutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz,r, g, b, a));
+
+                }
+
+                    else if(token.compare("cutellipsoid")==0){
+                        int xcenter, ycenter, zcenter, rx, ry, rz;
+                        ss >> xcenter >> ycenter >> zcenter >> rx >> ry >> rz;
+                        Figuras.push_back(new CutEllipsoid(xcenter, ycenter, zcenter, rx, ry, rz));
+
+                }
+
 
 
     }
